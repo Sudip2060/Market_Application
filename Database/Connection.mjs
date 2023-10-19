9ca1c2fdf5eab10d8fc8f6271319ced67e634128
Sudip2060 <sudipghimire9844@gmail.com>
2023-10-18 20:02:31 -0400

    Creating Database Connection File
    The Connection file for database 'Connection.mjs' has been created under a separate folder named 'Database'.
    app.js has been updated with necessary line of codes to display the welcome message as per the requirements.

--- Database/Connection.mjs
+++ Database/Connection.mjs
@@ -1,2 +1,31 @@
-import mongo from 'mongodb';
+        //importing the required modules for mongodb including the MongoClient Class
+        import mongo, { MongoClient } from 'mongodb';
+
+        //Define the connection string for Mongodb
+        const connectionstring = 'mongodb+srv://admin:admin@admin.bby89at.mongodb.net/?retryWrites=true&w=majority';
+
+        //Create a new instance of the mongoclient using the connection string
+        const client = new MongoClient(connectionstring);
+        let connstatus;
+
+
+        try{
+            //Establishing a connection with the Mongodb database
+            connstatus = await client.connect();
+            console.log("Database is connected Sucessfully")
+        }
+        catch (er){
+                console.log(er);
+        }
+
+        
+       // Define the collection name
+        const collectionName = 'Product';
+
+        // Access the "product" collection
+        const db = client.db('Marketplace');
+        const productCollection = db.collection(collectionName);    
+
+      
+
 
